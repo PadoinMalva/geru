49aa6cc58af37bc1ce7f3393025d3b6d2e990f2e
Daniel Padoin <dpmalva@gmail.com>
2019-06-30 21:01:37 -0300

    endpoint get_logs and decorator to check request.method

--- geru_challange/geru_challange/helpers/identifier/identifier.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-strlen = 32
-
-def random_string():
-    """Generate a random string with the combination of lowercase and uppercase letters """
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for i in range(strlen))
